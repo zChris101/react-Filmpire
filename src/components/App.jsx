383f0deeccce76f6bf9659efa9be955174f9697f
ChristianMusico <chrstnmusico@gmail.com>
2025-01-31 04:36:43 +0800

    initila commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {CssBaseline} from '@mui/material'
+//import {Route, Routes} from 'react-router-dom'
+
+const App = () => {
+  return (
+    <div>
+      <CssBaseline />
+      <h1>Hello World - Filmpire</h1>
+    </div>
+  )
+}
+ 
+export default App;
